d26de0dcfbed6087eebc9903de149c3754d187c5
Assen Kolov <a.kolov@newmotion.com>
2020-01-16 18:47:35 +0100

    minor changes

--- gitlab-scripts/get_scripts.sh
+++ gitlab-scripts/get_scripts.sh
@@ -2,3 +2,6 @@
 
 curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/create_ivy_credentials.sh > gitlab_scripts/create_ivy_credentials.sh
 chmod +x gitlab_scripts/create_ivy_credentials.sh
+
+curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/create_ivy_credentials.sh > gitlab_scripts/copy_ivy_credentials.sh
+chmod +x gitlab_scripts/copy_ivy_credentials.sh
