e4dc228ac58c08313d21e1bc61f804467d71bd75
Assen Kolov <a.kolov@newmotion.com>
2020-01-24 12:24:35 +0100

    wip

--- gitlab-scripts/get_scripts.sh
+++ gitlab-scripts/get_scripts.sh
@@ -3,8 +3,8 @@
 curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/create_ivy_credentials.sh > gitlab_scripts/create_ivy_credentials.sh
 chmod +x gitlab_scripts/create_ivy_credentials.sh
 
-curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/create_ivy_credentials.sh > gitlab_scripts/copy_ivy_credentials.sh
+curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/copy_ivy_credentials.sh > gitlab_scripts/copy_ivy_credentials.sh
 chmod +x gitlab_scripts/copy_ivy_credentials.sh
 
-curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/create_ivy_credentials.sh > gitlab_scripts/notify_slack_channel.sh
+curl https://raw.githubusercontent.com/kolov/publish-stuff/master/gitlab-scripts/notify_slack_channel.sh > gitlab_scripts/notify_slack_channel.sh
 chmod +x gitlab_scripts/notify_slack_channel.sh
