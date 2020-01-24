138385dfd52f0ebf82a9d03077717c8e089c8e44
Assen Kolov <a.kolov@newmotion.com>
2020-01-24 13:12:23 +0100

    wip

--- gitlab-scripts/notify_slack_channel.sh
+++ gitlab-scripts/notify_slack_channel.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo $SLACK_MSG_FAILED > slack-message-template.json
+cat slack-message-template.json
 sed -e "s/APP_NAME/$CI_PROJECT_NAME/" -e "s/BRANCH/$CI_COMMIT_REF_NAME/" -e "s/STAGE/$CI_JOB_STAGE/" slack-message-template.json > slack-message.json
 cat slack-message.json
 echo Will Post to $SLACK_POST_URL
